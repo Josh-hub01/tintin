04ff2b917facfd7c874051b548a2ad313b289a19
josh <bronzetrees16@163.com>
2024-10-31 16:47:26 +0800

    update

--- src/Auction.sol
+++ src/Auction.sol
@@ -3,36 +3,63 @@ pragma solidity ^0.8.20;
 
 contract Auction {
   address payable public beneficiary;
-  uint public auctionEnd;
+  uint public auctionEndTime;
+
+  // Current state of the auction.
   address public highestBidder;
   uint public highestBid;
+
+  // Allowed withdrawals of previous bids
   mapping(address => uint) public pendingReturns;
   bool ended;
+
+  mapping(address => uint) public prevTimes;
+  uint public cooldownTime = 10 minutes;
+  uint public extendedTime = 5 minutes;
+  uint public startExtendedTime = 5 minutes;
 
   event HighestBidIncreased(address bidder, uint amount);
   event AuctionEnded(address winner, uint amount);
 
-  constructor(
-    uint biddingTime_,
-    address payable beneficiary_
-  ) {
+  error AuctionAlreadyEnded();
+  error BidNotHighEnough(uint highestBid);
+  error CooldownPeriodNotEnded(uint prevTime);
+  error AuctionNotYetEnded();
+
+  constructor(uint biddingTime_, address payable beneficiary_) {
     beneficiary = beneficiary_;
-    auctionEnd = block.timestamp + biddingTime_;
+    auctionEndTime = block.timestamp + biddingTime_;
   }
 
-  function bid() public payable {
-    require(block.timestamp <= auctionEnd, "Auction already ended");
-    require(msg.value > highestBid, "There already is a higher bid");
+  function bid() external payable {
+    if (block.timestamp > auctionEndTime) {
+      revert AuctionAlreadyEnded();
+    }
+    if (msg.value <= highestBid) {
+      revert BidNotHighEnough(highestBid);
+    }
+    if (block.timestamp < (prevTimes[msg.sender] + cooldownTime)) {
+      revert CooldownPeriodNotEnded(prevTimes[msg.sender]);
+    }
 
-    if (highestBid!= 0) {
+    // Update the previous cooldown time for the sender
+    prevTimes[msg.sender] = block.timestamp;
+
+    if (highestBid != 0) {
       pendingReturns[highestBidder] += highestBid;
     }
+
     highestBid = msg.value;
     highestBidder = msg.sender;
     emit HighestBidIncreased(msg.sender, msg.value);
+
+    // Extend the auction time if it's within the last extension period
+    if (block.timestamp >= auctionEndTime - startExtendedTime) {
+      auctionEndTime += extendedTime;
+    }
   }
 
-  function withdraw() public returns (bool) {
+  function withdraw() external returns (bool) {
     uint amount = pendingReturns[msg.sender];
     if (amount > 0) {
       pendingReturns[msg.sender] = 0;
@@ -41,12 +68,18 @@ contract Auction {
     return true;
   }
 
-  function endAuction() public {
-    require(block.timestamp >= auctionEnd, "Auction has not ended yet");
-    require(!ended, "Auction has already ended");
+  function endAuction() external {
+    if (block.timestamp < auctionEndTime) {
+      revert AuctionNotYetEnded();
+    }
+
+    if (ended) {
+      revert AuctionAlreadyEnded();
+    }
 
     ended = true;
     emit AuctionEnded(highestBidder, highestBid);
+    
     beneficiary.transfer(highestBid);
   }
 }
